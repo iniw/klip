88277d4c4b8a7a8bd5ed234a794b9c95ea3459c0
Vinicius Deolindo <andrade.vinicius934@gmail.com>
2025-06-30 15:32:56 -0300

    flake: update nixpkgs and remove devshell

--- flake.nix
+++ flake.nix
@@ -1,18 +1,13 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      rust-overlay,
       flake-utils,
     }:
     {
@@ -23,51 +18,25 @@
     // flake-utils.lib.eachDefaultSystem (
       system:
       let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
-        rust-toolchain = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [
-            "rust-analyzer"
-            "rust-src"
-          ];
-        };
-        buildInputs =
-          with pkgs;
-          lib.optionals stdenv.isDarwin [
-            darwin.apple_sdk.frameworks.AppKit
-          ];
-
+        pkgs = import nixpkgs { inherit system; };
       in
       {
-        devShells.default =
-          with pkgs;
-          mkShell {
-            packages = [ rust-toolchain ];
-
-            inherit buildInputs;
-          };
-
         packages = rec {
-          klip =
-            with pkgs;
-
-            rustPlatform.buildRustPackage {
-              name = "klip";
-              src = ./.;
-              cargoLock = {
-                lockFile = ./Cargo.lock;
-              };
-
-              meta = {
-                description = "A dead simple cross-platform tool to interact with the clipboard";
-                homepage = "https://github.com/iniw/klip";
-                license = [
-                  lib.licenses.bsd2
-                ];
-              };
-
-              inherit buildInputs;
+          klip = pkgs.rustPlatform.buildRustPackage {
+            name = "klip";
+            src = ./.;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
             };
+
+            meta = {
+              description = "A dead simple cross-platform tool to interact with the clipboard";
+              homepage = "https://github.com/iniw/klip";
+              license = [
+                pkgs.lib.licenses.bsd2
+              ];
+            };
+          };
 
           default = klip;
         };
